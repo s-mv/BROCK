b68874f8b41868729c4794706c9b69708cc7fa40
s-mv <shreerangvaidya28@gmail.com>
2020-08-09 10:59:26 +0530

    Added some functions, not ready yet.

--- brock.js
+++ brock.js
@@ -5,48 +5,39 @@
   Version: 0.1.0
   Liscence: "Just-credit-me-when-you-use-this-file-please-thanks" 
 */
+
 function BROCK() {
+  function parseGaryArray(gary = '') {
+    if (gary[0] != '[') return;
+    let arr = gary.split(/[\[\],]/gi).join('');
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+      const c = arr[i];
+      
+    }
+  }
   this.parseGary = (gary) => {
     let p;
-    let lc = '';
-    let mc = '';
-    let mcc = [];
-    let layer = 0;
     for (let i = 0; i < gary.length; i++) {
       const c = gary[i];
       switch (c) {
-        case '/':
-          if (lc = '/') {
-            if (mc == 'comment') {
-              mc = '';
-              mcc[layer] = i;
-            } else {
-              mc = 'comment';
-              mcc[layer] = i - 1;
+        case '[':
+          let k = i;
+          let done;
+          for (let j = i + 1; j < gary.length; j++) {
+            const c2 = gary[j];
+            i++;
+            if(c2 == ']') {
+              let array = parseGaryArray(gary);  
+              done = true;
             }
           }
-          break;
-        case '(':
-          // if (mc == 'object') throw ("BrockError at char'," + i);
-          mc == 'object';
-          layer++;
-          break;
-        case ')':
-          if (mc = 'object'){
-            layer--;
-            mcc[layer] = 1;
-            if(layer = 0);
-          }
+          if(!done) throw `BrockError: [ at ${k} not closed`;
           break;
         default:
-          if (mc != 'comment') throw `BrockError: cannot parse char ${i}`;
-        case ' ':
-        case '\n':
           break;
       }
-      lc = c;
     }
-    // return 'not ready yet';
     return p;
   }
   garyToJSON = (gary) => {
